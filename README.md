edb8c05cf4944cef8a0c92afea88daa3952ff098
tetor <tetor.takashi.etou@gmail.com>
2017-03-24 02:55:12 +0900

    Cat file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # transer
 
 `stack build` したら、`.stack-work` 以下になんかできる。そこにbinもあると思う
+
+`stack exec transer-exe file_name` で実行
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,14 @@ module Lib
     ( someFunc
     ) where
 
+import System.Environment
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  (x:xs) <- getArgs
+  putFile x
+
+putFile :: String -> IO ()
+putFile f = do
+  cs <- readFile f
+  putStr cs
