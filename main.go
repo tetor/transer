c47278710b1d0f4ddde09dd10659368f637e2265
tetor <tetor.takashi.etou@gmail.com>
2017-08-03 10:27:08 +0900

    transer

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-	"github.com/tetor/transer/runner"
+	"github.com/tetor/transer/cmd"
 )
 
 func main() {
-	if err := runner.RootCmd.Execute(); err != nil {
+	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
